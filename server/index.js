09d2b58df95641c0a004830f46146f6a788716a4
abdullahWeb-Dev <163756430+abdullahWeb-Dev@users.noreply.github.com>
2024-05-09 11:16:01 +0500

    d

--- server/index.js
+++ server/index.js
@@ -42,7 +42,13 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors());
+app.use(
+  cors({
+    origin: ["admin-dashboard-api-sable.vercel.app/"],
+    methods: ["GET"],
+    credentials: true,
+  })
+);
 
 /**
  * *ROUTES
